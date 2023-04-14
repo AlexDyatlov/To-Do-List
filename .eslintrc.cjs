4b836a8bd7416b584b6ce350b9a7c48be88b6465
AlexDyatlov <aleks78.gooogle@gmail.com>
2023-04-14 10:31:22 +0300

    Added Tailwind CSS, ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    indent: ['error', 2],
+    semi: [2, 'always'],
+    'react/prop-types': 'off',
+    'eol-last': ['error', 'always'],
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'always', named: 'never' }
+    ],
+    'multiline-ternary': 'off',
+    quotes: ['error', 'single'],
+    'react/react-in-jsx-scope': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Title</h1>
+      <h1 className="text-red-600">Title</h1>
     </div>
   );
 }
